65c6e9ddb04ee67f139e9126c65b47b78abdfdad
Matt Dorsett <matthew.a.dorsett@gmail.com>
2014-05-06 22:00:12 -0700

    Tweaked algorithm

--- NumberClassification/src/NumberClassification.cpp
+++ NumberClassification/src/NumberClassification.cpp
@@ -16,9 +16,12 @@
 #include <opencv/cvwimage.h>
 #include <vector>
 #include <cstring>
+#include <tesseract/baseapi.h>
+#include <tesseract/strngs.h>
 
 using namespace cv;
 using namespace std;
+using namespace tesseract;
 
 const char* SEGMENT_NAMES[] = { "top", "middle", "bottom",
                                 "top_left", "top_right",
@@ -35,6 +38,7 @@ void checkSegment(char& guess, const char& segment, Mat img, int x, int y, int w
   Mat cropped = img(r);
   if (TOTAL_PIXELS - countNonZero(cropped) > PIXEL_THRESHOLD)
     guess |= segment;
+  
   if (segment == TOP) {
     imwrite(string("./segments/top/") + img_name, cropped);
   } else if (segment == MIDDLE) {
@@ -73,8 +77,6 @@ int predictNumber(const char& guess) {
       return 2;
     case FIVE_SEGMENTS:
       return 5;
-    case FOUR_SEGMENTS_ERR:
-      return 4;
     case FOUR_SEGMENTS:
       return 4;
     case SEVEN_SEGMENTS:
@@ -85,17 +87,6 @@ int predictNumber(const char& guess) {
       return 1;
     default:
       return -1;
-  }
-}
-
-void cleanBorder(Mat img_bin, int width, int height) {
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      if (i < 1 || i > height-2)
-        img_bin.at<uchar>(i, j, 0) = 255;
-      else if (j < 1 || j > width-2)
-        img_bin.at<uchar>(i, j, 0) = 255;
-    }
   }
 }
 
@@ -128,47 +119,42 @@ int main(int argc, char** argv) {
       int thresh = (int)mean(channels[0])[0];    
       Mat img_bin(img_gray.size(), img_gray.type());
       threshold(img_gray, img_bin, thresh, 255, THRESH_BINARY);
-      cleanBorder(img_bin, 20, 30);
       imwrite(string("./bin/") + img_name, img_bin);
+      /*bitwise_not(img_bin, img_bin);
+      TessBaseAPI tess;
+      tess.Init(NULL, "eng", OEM_DEFAULT);
+      tess.SetPageSegMode(PSM_SINGLE_BLOCK);
+      tess.SetImage((uchar*)img_bin.data, img_bin.cols, img_bin.rows, 1, img_bin.cols);
+      tess.SetVariable("tessedit_char_whitelist", "0123456789");
+      
+      char* out = tess.GetUTF8Text();
+      cout << out << endl;
+    }
+  }*/
       checkSegment(guess, TOP, img_bin, 9, 1, 2, 8, img_name);
       checkSegment(guess, MIDDLE, img_bin, 9, 11, 2, 8, img_name);
-      checkSegment(guess, BOTTOM, img_bin, 9, 21, 2, 8, img_name);
+      checkSegment(guess, BOTTOM, img_bin, 9, 22, 2, 8, img_name);
       checkSegment(guess, TOP_LEFT, img_bin, 1, 9, 8, 2, img_name);
       checkSegment(guess, TOP_RIGHT, img_bin, 11, 9, 8, 2, img_name);
-      checkSegment(guess, BOTTOM_LEFT, img_bin, 1, 19, 8, 2, img_name);
-      checkSegment(guess, BOTTOM_RIGHT, img_bin, 11, 19, 8, 2, img_name);
+      checkSegment(guess, BOTTOM_LEFT, img_bin, 1, 20, 8, 2, img_name);
+      checkSegment(guess, BOTTOM_RIGHT, img_bin, 11, 20, 8, 2, img_name);
       int n = predictNumber(guess);      
       guesses[img_num]++;
       if (n < 0) {
         cout << "Could not predict number for " << img_name << endl;
+        cout << "Guessed: " << (int)guess << endl;
       } else {
         //cout << "Predicted: " << n << endl;
         if (n == img_num)
           correct[n]++;
-        else
+        else {
           cout << "Could not predict number for " << img_name << endl;
-      }
-    }
-  }
-  /*n = scandir("./numbers", &list, filter, alphasort);
-  if (n < 0) {
-    cerr << "ERROR: Bad directory" << endl;
-  } else {
-    for (int i = 0; i < N_SEGMENT_TYPES; i++) {
-      string dir_name = string("./segments/") + SEGMENT_NAMES[i] + "/";
-      for (int j = 0; j < n; j++) {
-        string img_name(list[j]->d_name);
-        int img_num = img_name.at(0) - 0x30;
-        cout << "Processing file for #" << img_num << ": " << dir_name + img_name << endl;
-        Mat img = imread(dir_name + img_name, CV_LOAD_IMAGE_COLOR);
-        if (img.empty()) {
-          cerr << "ERROR: could not read image " << img_name << endl;
+          cout << "Guessed: " << (int)guess << endl;
         }
       }
     }
-
-  }*/
-
+  }
+  
   int total_correct = 0;
   int total_guesses = 0;
   for (int i = 0; i < 10; i++) {
